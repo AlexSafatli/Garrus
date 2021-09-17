7bcaec6343737fed9acdddf767879a274051e7b0
Alex Safatli <safatli@cs.dal.ca>
2021-09-17 00:51:42 -0300

    Refactor command routing to use less handlers

--- bot.go
+++ bot.go
@@ -12,7 +12,9 @@ import (
 
 // Bot encompasses a DiscordGo Bot
 type Bot struct {
-	Start time.Time
+	Start           time.Time
+	MessageCommands []*chat.MessageCommand
+	SlashCommands   []*chat.SlashCommand
 	*discordgo.Session
 }
 
@@ -23,13 +25,50 @@ func NewBot(token string) (b *Bot, err error) {
 		return
 	}
 	b = &Bot{Start: time.Now(), Session: discord}
+	b.initMessageCommands()
+	b.initSlashCommands()
 	b.routeHandlers()
+	if err = b.registerSlashCommands(); err != nil {
+		return
+	}
 	return
 }
 
+func (b *Bot) initMessageCommands() {
+	b.MessageCommands = []*chat.MessageCommand{
+		{
+			Command:  ".about",
+			Function: commands.AboutMessageCommand,
+		},
+	}
+}
+
+func (b *Bot) initSlashCommands() {
+	b.SlashCommands = []*chat.SlashCommand{
+		{
+			Command: &discordgo.ApplicationCommand{
+				Name:        "about",
+				Description: "About this bot",
+			},
+			Function: commands.AboutSlashCommand,
+		},
+	}
+}
+
 func (b *Bot) routeHandlers() {
-	b.AddHandler(chat.NewHandler(b.Session, ".about", commands.AboutCommand))
+	b.AddHandler(chat.NewMessageCommandRouteHandler(b.Session, b.MessageCommands))
+	b.AddHandler(chat.NewSlashCommandRouteHandler(b.Session, b.SlashCommands))
 	b.AddHandler(chat.DiceRollHandler)
+}
+
+func (b *Bot) registerSlashCommands() error {
+	for _, c := range b.SlashCommands {
+		_, err := b.Session.ApplicationCommandCreate(b.Session.State.User.ID, "", c.Command)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Self returns the User struct associated with the bot user
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
-	github.com/bwmarrin/discordgo v0.23.1
+	github.com/bwmarrin/discordgo v0.23.3-0.20210821175000-0fad116c6c2a
 	github.com/evalphobia/go-config-loader v0.1.3
+	github.com/stretchr/testify v1.7.0 // indirect
 )
